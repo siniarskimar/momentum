8455659ef07d1a2b9dd59572643eb4f5d2625c1c
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-01-05 16:39:14 +0100

    Split types.d.ts

---
+++ src/lib/calendar.d.ts
@@ -0,0 +1,9 @@
+
+  export interface Event {
+    title: string,
+    description: string,
+    tags: string[],
+    date: number,
+    end_date: number,
+    periodic_interval: number | null,
+  };
--- src/lib/types.d.ts
+++
@@ -1,1 +0,0 @@
-export type CalendarView = 'month' | 'week';
