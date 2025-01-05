f2e496ae998eafb77bef03c281030ad4d66be34d
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-01-05 18:18:33 +0100

    Change global styles

--- src/lib/Calendar.svelte
+++ src/lib/Calendar.svelte
@@ -1,96 +1,89 @@
 <script>
-    import MonthView from "./calendar/MonthView.svelte";
-    import { DateTime } from "luxon";
+  import MonthView from "./calendar/MonthView.svelte";
+  import { DateTime } from "luxon";
 
-    /** @type {{
-     *      view?: import('$lib/ui').CalendarView,
-     *      date?: import('luxon').DateTime
-     *  }}
-     */
-    let { view = $bindable("month"), date = $bindable(DateTime.now()) } =
-        $props();
+  /** @type {{
+   *      view?: import('$lib/ui').CalendarView,
+   *      date?: import('luxon').DateTime
+   *  }}
+   */
+  let { view = $bindable("month"), date = $bindable(DateTime.now()) } =
+    $props();
 </script>
 
 <div class="container">
-    <div class="topbar">
-        <div class="viewselect">
-            <button
-                class:view-selected={view == "month"}
-                onclick={() => {
-                    view = "month";
-                }}>M</button
-            >
-            <button
-                class:view-selected={view == "week"}
-                onclick={() => {
-                    view = "week";
-                }}>W</button
-            >
-        </div>
-        <p class="currdate">
-            {date.toJSDate().toLocaleDateString(undefined, {
-                month: "long",
-                year: "numeric",
-            })}
-        </p>
-        <div class="spacer"></div>
+  <div class="topbar">
+    <div class="viewselect">
+      <button
+        class:view-selected={view == "month"}
+        onclick={() => {
+          view = "month";
+        }}>M</button
+      >
+      <button
+        class:view-selected={view == "week"}
+        onclick={() => {
+          view = "week";
+        }}>W</button
+      >
     </div>
-    {#if view === "month"}
-        <MonthView bind:viewDate={date} />
-    {:else if view == "week"}
-        <p>Week view</p>
-    {/if}
+    <p class="currdate">
+      {date.toJSDate().toLocaleDateString(undefined, {
+        month: "long",
+        year: "numeric",
+      })}
+    </p>
+    <div class="spacer"></div>
+  </div>
+  {#if view === "month"}
+    <MonthView bind:viewDate={date} />
+  {:else if view == "week"}
+    <p>Week view</p>
+  {/if}
 </div>
 
 <style>
-    .container {
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        height: 100%;
-    }
+  .container {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+  }
 
-    .topbar {
-        display: flex;
-    }
+  .topbar {
+    display: flex;
+  }
 
-    .topbar > * {
-        flex: 1;
-    }
+  .topbar > * {
+    flex: 1;
+  }
 
-    @media (prefers-color-scheme: dark) {
-        .topbar {
-            color: #ffffff;
-            background-color: #0f0f0f98;
-        }
-    }
+  .viewselect {
+    display: flex;
+    flex-direction: row;
+    justify-content: start;
+    align-items: center;
+  }
 
-    .viewselect {
-        display: flex;
-        flex-direction: row;
-        justify-content: start;
-        align-items: center;
-    }
+  .viewselect button {
+    border-radius: 0;
+    border-top-left-radius: 0.2em;
+    border-top-right-radius: 0.2em;
+    border: 1px solid rgba(0, 0, 0, 0.2);
 
-    .viewselect button {
-        border-radius: 0;
-        border-top-left-radius: 0.2em;
-        border-top-right-radius: 0.2em;
-        border: 1px solid rgba(0, 0, 0, 0.2);
+    margin-left: 0.3rem;
+    margin-right: 0.3rem;
+    padding-top: 0.5rem;
+    padding-bottom: 0;
+  }
 
-        margin-left: 0.3rem;
-        margin-right: 0.3rem;
-        padding-top: 0.5rem;
-        padding-bottom: 0;
-    }
+  .viewselect .view-selected {
+    background-color: var(--color-accent);
+  }
 
-    .viewselect .view-selected {
-        background-color: chocolate;
-    }
+  .currdate {
+    text-align: center;
 
-    .currdate {
-        text-align: center;
-
-        margin: 0;
-    }
+    margin: 0;
+  }
 </style>
--- src/lib/calendar/MonthView.svelte
+++ src/lib/calendar/MonthView.svelte
@@ -1,134 +1,141 @@
 <script>
-    import { DateTime, Interval } from "luxon";
+  import { DateTime, Interval } from "luxon";
 
-    /** @type {{viewDate?: DateTime }} */
-    let { viewDate = $bindable(DateTime.now()) } = $props();
+  /** @type {{viewDate?: DateTime }} */
+  let { viewDate = $bindable(DateTime.now()) } = $props();
 
-    const startOfWeek = viewDate.startOf("week");
-    const endOfWeek = viewDate.endOf("week");
-    const firstVisibleDay = viewDate.startOf("month").startOf("week");
-    const lastVisibleDay = viewDate.endOf("month").endOf("week");
+  const startOfWeek = viewDate.startOf("week");
+  const endOfWeek = viewDate.endOf("week");
+  const firstVisibleDay = viewDate.startOf("month").startOf("week");
+  const lastVisibleDay = viewDate.endOf("month").endOf("week");
 
-    const visibleDays = /** @type{DateTime[]} */ (Interval.fromDateTimes(firstVisibleDay, lastVisibleDay)
+  const visibleDays = /** @type{DateTime[]} */ (
+    Interval.fromDateTimes(firstVisibleDay, lastVisibleDay)
+      .splitBy({ days: 1 })
+      .map((d) => d.start)
+  );
+
+  const visibleWeeks = /** @type{DateTime[]} */ (
+    Interval.fromDateTimes(firstVisibleDay, lastVisibleDay)
+      .splitBy({ weeks: 1 })
+      .map((i) => i.start)
+  );
+
+  const daysOfCurrWeek = Interval.fromDateTimes(startOfWeek, endOfWeek)
+    .splitBy({ days: 1 })
+    .map((d) => d.start);
+
+  /**
+   * @param {DateTime} start
+   * @returns {DateTime[]}
+   */
+  function daysInWeek(start) {
+    return /** @type {DateTime[]}*/ (
+      Interval.fromDateTimes(start.startOf("week"), start.endOf("week"))
         .splitBy({ days: 1 })
-        .map((d) => d.start));
-
-    const visibleWeeks = /** @type{DateTime[]} */ (Interval.fromDateTimes(firstVisibleDay, lastVisibleDay)
-        .splitBy({weeks: 1})
-        .map((i) => i.start));
-
-    const daysOfCurrWeek = Interval.fromDateTimes(startOfWeek, endOfWeek)
-        .splitBy({ days: 1 })
-        .map((d) => d.start);
-
-    /**
-     * @param {DateTime} start
-     * @returns {DateTime[]}
-     */
-    function daysInWeek(start) {
-        return /** @type {DateTime[]}*/ (Interval.fromDateTimes(start.startOf('week'), start.endOf('week'))
-            .splitBy({days: 1})
-            .map((i) => i.start));
-    }
-    
+        .map((i) => i.start)
+    );
+  }
 </script>
 
 <div class="container">
-    <div class="topbar">
-        {#each daysOfCurrWeek as day}
-            <p class="weekday">
-                {day
-                    ?.toJSDate()
-                    .toLocaleDateString(undefined, { weekday: "short" })}
-            </p>
-        {/each}
-    </div>
-    {#each visibleWeeks as visibleWeek}
-        <div class="day-row" role="row">
-            {#each daysInWeek(visibleWeek) as day}
-                <div class="day" class:exclusive={day?.month != viewDate.month}>
-                    <button class="day-number">{day?.day}</button>
-                </div>
-            {/each}
-        </div>
+  <div class="topbar">
+    {#each daysOfCurrWeek as day}
+      <p class="weekday">
+        {day?.toJSDate().toLocaleDateString(undefined, { weekday: "short" })}
+      </p>
     {/each}
+  </div>
+  {#each visibleWeeks as visibleWeek}
+    <div class="day-row" role="row">
+      {#each daysInWeek(visibleWeek) as day}
+        <div class="day" class:exclusive={day?.month != viewDate.month}>
+          <button class="day-number">{day?.day}</button>
+        </div>
+      {/each}
+    </div>
+  {/each}
 </div>
 
 <style>
-    .container {
-        position: relative;
-        height: 100%;
-        
-        display: flex;
-        flex-direction: column;
-    }
+  .container {
+    position: relative;
+    height: 100%;
 
+    display: flex;
+    flex-direction: column;
+  }
+
+  .topbar {
+    position: sticky;
+    top: 0;
+
+    display: flex;
+    flex-direction: row;
+
+    z-index: 2;
+    background-color: hsl(0, 0%, 100%);
+  }
+
+  .topbar > * {
+    user-select: none;
+    flex: 1;
+  }
+
+  @media (prefers-color-scheme: dark) {
     .topbar {
-        position: sticky;
-        top: 0;
-
-        display: flex;
-        flex-direction: row;
-
-        z-index: 2;
-        background-color: hsl(0, 0%, 100%);
+      color: #ffffff;
+      background-color: #0f0f0f98;
     }
+  }
 
-    .topbar > * {
-        user-select: none;
-        flex: 1;
-    }
+  .topbar > p {
+    margin: 0;
+    height: 100%;
+    text-align: center;
+    box-shadow:
+      1px 0px 0px 0px rgba(0, 0, 0, 0.1),
+      -1px 0px 0px 0px rgba(0, 0, 0, 0.1);
+  }
 
-    @media (prefers-color-scheme: dark) {
-        .topbar {
-            color: #ffffff;
-            background-color: #0f0f0f98;
-        }
-    }
+  .day-row {
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+  }
 
-    .topbar > p {
-        margin: 0;
-        height: 100%;
-        text-align: center;
-        box-shadow:
-            1px 0px 0px 0px rgba(0, 0, 0, 0.1),
-            -1px 0px 0px 0px rgba(0, 0, 0, 0.1);
-    }
+  .day {
+    flex: 1;
+    position: relative;
 
-    .day-row {
-        flex: 1;
-        display: flex;
-        flex-direction: row;
-    }
+    outline: 1px solid rgba(0, 0, 0, 0.1);
+    border-radius: 0;
+  }
 
-    .day {
-        flex: 1;
-        position: relative;
+  .day-number {
+    position: absolute;
+    bottom: 0.01rem;
+    left: 0.1rem;
 
-        outline: 1px solid rgba(0, 0, 0, 0.1);
-        border-radius: 0;
-    }
+    background: transparent;
+    border: none;
+    box-shadow: none;
 
-    .day-number {
-        position: absolute;
-        bottom: 0.01rem;
-        left: 0.1rem;
+    margin: 0;
+    user-select: none;
+    opacity: 0.6;
+    transition: 150ms ease-in-out opacity;
+  }
 
-        margin: 0;
-        user-select: none;
-        opacity: 0.6;
-        transition: 150ms ease-in-out opacity;
-    }
+  .day:hover .day-number {
+    opacity: 1;
+  }
 
-    .day:hover .day-number {
-        opacity: 1;
-    }
+  .day.exclusive {
+    background-color: rgba(80, 80, 80, 0.1);
+  }
 
-    .day.exclusive {
-        background-color: rgba(80, 80, 80, 0.1);
-    }
-
-    .day.exclusive .day-number {
-        opacity: 0.4;
-    }
+  .day.exclusive .day-number {
+    opacity: 0.4;
+  }
 </style>
