2f0af1466c8df086e7f3a9d187a73337649bf2f8
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-07-10 19:05:25 +0200

    Match Event struct to schema

--- src-tauri/src/model/event.rs
+++ src-tauri/src/model/event.rs
@@ -6,26 +6,13 @@ use serde::{Deserialize, Serialize};
 pub struct Event {
     pub id: u64,
     pub title: String,
-    pub description: String,
-    pub tags: Vec<String>,
-    #[serde(with = "ts_seconds")]
-    pub date: DateTime<chrono::Utc>,
-
-    #[serde(with = "ts_seconds")]
-    pub end_date: DateTime<chrono::Utc>,
 }
 
 impl Event {
-    pub fn new(title: &str, date: DateTime<chrono::Utc>) -> Self {
+    pub fn new(title: &str) -> Self {
         return Self {
             id: 0,
             title: String::from(title),
-            description: String::new(),
-            tags: vec![],
-            date,
-            end_date: date
-                .checked_add_signed(chrono::TimeDelta::hours(1))
-                .unwrap(),
         };
     }
 }
