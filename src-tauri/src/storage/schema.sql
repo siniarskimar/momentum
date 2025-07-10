26807dd5b2db54088fe15c5ccce1c002e012402e
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-07-10 19:00:19 +0200

    Create initial storage schema

---
+++ src-tauri/src/storage/schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE _master (
+    schema_version INTEGER NOT NULL
+)
+
+CREATE TABLE interval (
+    id INTEGER PRIMARY KEY,
+    start_at INTEGER NOT NULL,
+    end_at INTEGER NOT NULL,
+
+    ref_type INTEGER NOT NULL,
+    ref_id INTEGER NOT NULL,
+
+    -- 1 = event
+    -- 2 = task
+    -- 3 = reminder
+    CHECK (ref_type IN (1, 2, 3)),
+    UNIQUE (start_at, end_at, ref_type, ref_id)
+);
+
+CREATE TABLE event (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    completed INTEGER NOT NULL DEFAULT 0
+);
+
+CREATE TABLE reminder (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL  
+);
+
+
+INSERT INTO _master
+VALUES (
+    1
+);
