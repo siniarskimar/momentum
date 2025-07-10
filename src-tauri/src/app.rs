99122e77e4f9b2821bb543910be91313706677c0
Marcin Siniarski <15676482+siniarskimar@users.noreply.github.com>
2025-07-10 19:17:01 +0200

    Cease to create events and changelog directories

--- src-tauri/src/app.rs
+++ src-tauri/src/app.rs
@@ -7,25 +7,9 @@ use crate::storage::SqliteStorage;
 pub fn setup_local_datadir(app: &mut tauri::App) -> anyhow::Result<()> {
     let datadir = get_local_datadir(&app)?;
 
-    let suffixes = &["events", "changelog"];
-
     let mut dir_builder = std::fs::DirBuilder::new();
     dir_builder.recursive(true);
-
-    for &suffix in suffixes {
-        let path = datadir.join(suffix);
-        println!("{:?}", path);
-
-        match std::fs::metadata(&path) {
-            Ok(_) => return Ok(()),
-            Err(err) => match err.kind() {
-                std::io::ErrorKind::NotFound => {
-                    dir_builder.create(&path)?;
-                }
-                _ => return Err(anyhow!(err).context("failed to stat local data directory")),
-            },
-        }
-    }
+    dir_builder.create(&datadir)?;
 
     return Ok(());
 }
